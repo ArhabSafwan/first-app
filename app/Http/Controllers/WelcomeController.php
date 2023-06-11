4bc43dee2732e8bf5fa565b71702ab985d634e40
arhab_safwan <asafwan72@gmail.com>
2023-06-11 16:48:57 +0600

    eloquent

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Student;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -11,13 +12,26 @@ class WelcomeController extends Controller
         
         // how to fetch data from database 
 
-        //  1 Using raw Query
+        //  1.Using raw Query
         // $users = DB::select('select * from users');
         //   dd($users);
         
-        //  2 query builder
-           $users = DB::table('users')->select(['name','email'])->whereNotNull('email')->orderBy('name')->get();
-           dd($users);
-        //  3 eloquent orm        
+        //  2.query builder
+        //  $users = DB::table('users')->select(['name','email'])->whereNotNull('email')->orderBy('name')->get();
+        //  dd($users);
+
+
+        //  3 eloquent orm   
+        // $students = Student::select(['name','email'])->whereNotNull('email')->orderBy('name')->get();;
+        // dd($students);    
+    //     foreach ($students as $student) {
+    //         echo $student->name."<br>";
+    //     }     
+    
+    //insertion
+    // $student = new Student();
+    // $student->name ="NEW";
+    // $student->email = "new@gmail.com";
+    // $student->save();
     }
 }
